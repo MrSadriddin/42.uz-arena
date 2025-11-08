8bb5f4bb97a4699bbaf2dfb4444e2b48954fb906
Sadriddin Axhmadullayev <sadriddinaxhmadullayev@MacBook-Pro-Sadriddin.local>
2025-11-08 16:34:40 +0500

    Adding moveZeroes algoritm

--- javascript/moveZeroes/index.js
+++ javascript/moveZeroes/index.js
@@ -1,1 +1,28 @@
-console.log('first')
+// Nollarni orqaga surish
+// Berilgan sonlar ro'yxatidagi nollarni ro'yxat orqasiga o'tkazing, lekin boshqa elementlar ketma-ketligi buzilmasin.
+
+// Imkon qadar kamroq amal bajaring.
+
+// Qo'shimcha xotiradan foydalanmang - amallarni ro'yxat ustida bajaring.
+
+// Misol 1:
+// Kiritish: nums = [0,1,0,3,12]
+// Natija: [1, 3, 12, 0, 0]
+// Misol 1:
+// Kiritish: nums = [0]
+// Natija: [0]
+
+function moveZeroes(nums) {
+	let noNull = 0
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== 0) {
+			;[nums[noNull], nums[i]] = [nums[i], nums[noNull]]
+			noNull++
+		}
+	}
+	return nums
+}
+
+nums = [0, 1, 0, 3, 12]
+const console1 = moveZeroes(nums)
+console.log(console1)
