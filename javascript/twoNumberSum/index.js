a85f409644c038df5c42df42be4ba494adc7dacd
Sadriddin Axhmadullayev <sadriddinaxhmadullayev@MacBook-Pro-Sadriddin.local>
2025-11-08 16:18:47 +0500

    Adding twoNumberSum algoritm

--- javascript/twoNumberSum/index.js
+++ javascript/twoNumberSum/index.js
@@ -1,1 +1,16 @@
-console.log('salom')
+function twoNumberSum(array, targetSum) {
+	const nums = {}
+	for (let num of array) {
+		const complement = targetSum - num
+		if (nums[complement]) {
+			return complement < num ? [complement, num] : [num, complement]
+		}
+		nums[num] = true
+	}
+	return []
+}
+
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 10
+const console1 = twoNumberSum(array, targetSum)
+console.log(console1)
